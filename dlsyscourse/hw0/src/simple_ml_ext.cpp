93b9b9d68d6b1b358c29091e89e4c7f67050368f
PikaGu <icpcpikagu@gmail.com>
2023-11-17 17:07:38 +0800

    hw0 Q5

--- dlsyscourse/hw0/src/simple_ml_ext.cpp
+++ dlsyscourse/hw0/src/simple_ml_ext.cpp
@@ -6,6 +6,25 @@
 namespace py = pybind11;
 
 
+void matmul(const float* a, const float* b, float* C, size_t m, size_t k, size_t n) {
+    for (size_t row = 0; row < m; row++) {
+        for (size_t col = 0; col < n; col++) {
+            C[row * n + col] = 0;
+            for (size_t i = 0; i < k; i++) {
+                C[row * n + col] += a[row * k + i] * b[i * n + col];
+            }
+        }
+    }
+}
+
+void transpose(const float* in, float* out, size_t m, size_t n) {
+    for (size_t row = 0; row < m; row++) {
+        for (size_t col = 0; col < n; col++) {
+            out[col * m + row] = in[row * n + col];
+        }
+    }
+}
+
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 								  float *theta, size_t m, size_t n, size_t k,
 								  float lr, size_t batch)
@@ -33,7 +52,44 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      */
 
     /// BEGIN YOUR CODE
+    float* Z = new float[batch * k];
+    float* grad = new float[batch * k];
+    float* theta_grad = new float[n * k];
+    float* T = new float[n * batch];
 
+    size_t iter_num = m / batch;
+    for (size_t iter = 0; iter < iter_num; iter++) {
+        const float* iter_X = X + iter * batch * n;
+        const unsigned char* iter_y = y + iter * batch;
+
+        // batch*n X n*k
+        matmul(iter_X, theta, Z, batch, n, k);
+        for (size_t i = 0; i < batch; i++) {
+            float sum = 0;
+            for (size_t j = 0; j < k; j++) {
+                Z[i * k + j] = std::exp(Z[i * k + j]);
+                sum += Z[i * k + j];
+            }
+            for (size_t j = 0; j < k; j++) {
+                grad[i * k + j] = Z[i * k + j] / sum;
+                if (j == iter_y[i]) {
+                    grad[i * k + j] -= 1;
+                }
+                grad[i * k + j] /= batch;
+            }
+        }
+        transpose(iter_X, T, batch, n);
+        // n*batch X batch*k
+        matmul(T, grad, theta_grad, n, batch, k);
+        for (size_t i = 0; i < n*k; i++) {
+            theta[i] -= lr * theta_grad[i];
+        }
+    }
+
+    delete[] Z;
+    delete[] T;
+    delete[] grad;
+    delete[] theta_grad;
     /// END YOUR CODE
 }
 
